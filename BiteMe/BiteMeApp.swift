183ea03c094ac319cbe34c38fc8795d757abd8bc
abk <andrewking1597@gmail.com>
2025-10-11 12:35:52 -0400

    Initial Commit

---
+++ BiteMe/BiteMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteMeApp.swift
+//  BiteMe
+//
+//  Created by Andrew King on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BiteMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
