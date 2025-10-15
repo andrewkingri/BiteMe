ded554a3eb3334fb98be3f7cb66d37755a60570d
abk <andrewking1597@gmail.com>
2025-10-15 17:00:50 -0400

    edit profile updates

--- BiteMe/Core/UserProfile/EditUserProfileView.swift
+++ BiteMe/Core/UserProfile/EditUserProfileView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct EditUserProfileView: View {
     @Environment(\.dismiss) var dismiss
     
+    @State private var bio = "this is where my lil bio would go"
+    @State private var occupation = "my occupation"
+    
     let user: User
     
     var body: some View {
@@ -17,6 +20,82 @@ struct EditUserProfileView: View {
             ScrollView(showsIndicators: false) {
                 ImageGridView(user: user)
                     .padding()
+                
+                VStack(spacing: 24) {
+                    VStack(alignment: .leading) {
+                        Text("ABOUT ME")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .padding(.leading)
+
+                        // todo make this a custom TextEditor with a built in placeholder
+                        // (google "swift TextEditor placeholder ios 15")
+                        TextField("Add your bio", text: $bio)
+                            .padding()
+                            .frame(height: 64, alignment: .top)
+                            .background(Color(.secondarySystemBackground))
+                            .font(.subheadline)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("OCCUPATION")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .padding(.leading)
+                        
+                        HStack {
+                            Image(systemName: "book")
+                            
+                            Text("Occupation")
+                            
+                            Spacer()
+                            
+                            Text(occupation)
+                                .font(.footnote)
+                        }
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .font(.subheadline)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("GENDER")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .padding(.leading)
+                        
+                        HStack {
+                            Text("Man")
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .imageScale(.small)
+                        }
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .font(.subheadline)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("SEXUAL ORIENTATION")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .padding(.leading)
+                        
+                        HStack {
+                            Text("Straight")
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .imageScale(.small)
+                        }
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .font(.subheadline)
+                    }
+                }
             }
             .navigationTitle("Edit Profile")
             .navigationBarTitleDisplayMode(.inline)
