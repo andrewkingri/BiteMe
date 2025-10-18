62ebfbb16030bdfe9dffe87f292c1f8b02bbe2dc
abk <andrewking1597@gmail.com>
2025-10-18 16:05:22 -0400

    match screen view

---
+++ BiteMe/Core/Matches/View/RecipeMatchView.swift
@@ -0,0 +1,90 @@
+//
+//  RecipeMatchView.swift
+//  BiteMe
+//
+//  Created by Andrew King on 10/18/25.
+//
+
+import SwiftUI
+
+struct RecipeMatchView: View {
+    @Binding var show: Bool
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.black.opacity(0.7))
+//                .opacity(0.7)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 120) {
+                VStack {
+                    Image("its-a-match")
+                    
+                    Text("You saved recipe to your likes")
+                        .foregroundStyle(.white)
+                }
+                
+                HStack {
+                    Image("exampleProfileImg")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                        .overlay {
+                            Circle()
+                                .stroke(.white, lineWidth: 2)
+                                .shadow(radius: 4)
+                            
+                        }
+                    
+                    Image(MockData.recipes[2].imageURLs[0])
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                        .overlay {
+                            Circle()
+                                .stroke(.white, lineWidth: 2)
+                                .shadow(radius: 4)
+                            
+                        }
+                }
+                
+                VStack(spacing: 16) {
+                    Button("Add to List") {
+                        
+                    }
+                    .font(.subheadline)
+                    .font(Font.body.weight(.bold))
+                    .foregroundStyle(.white)
+                    .frame(width: 350, height: 44)
+                    .background(.pink)
+                    .clipShape(Capsule())
+                    
+                    Button("Keep Swiping") {
+                        
+                    }
+                    .font(.subheadline)
+                    .font(Font.body.weight(.bold))
+                    .foregroundStyle(.white)
+                    .frame(width: 350, height: 44)
+                    .background(.clear)
+                    .clipShape(Capsule())
+                    .overlay {
+                        Capsule()
+                            .stroke(.white, lineWidth: 1)
+                            .shadow(radius: 4)
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct RecipeMatchView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeMatchView(show: .constant(true))
+    }
+}
